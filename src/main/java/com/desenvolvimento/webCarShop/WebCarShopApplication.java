ad8afc1dfe50c44ff5ff4164db254b1169852ce9
Ariane Paim <arianeepaimm@gmail.com>
2023-10-17 15:08:01 -0300

    First commit

---
+++ src/main/java/com/desenvolvimento/webCarShop/WebCarShopApplication.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.webCarShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCarShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCarShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desenvolvimento/webCarShop/WebCarShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desenvolvimento.webCarShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCarShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
